b19f7e4035439bb2ddcc377185ac17eb52dd44c9
Myrotvorets <support@myrotvorets.center>
2024-02-03 18:44:26 +0200

    Add plugin header

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * Plugin Name: Move Attachments
+ * Description: Move attachments between posts
+ * Plugin URI: https://myrotvorets.center/
+ * Version: 1.0.0
+ * Author: Myrotvorets
+ * Author URI: https://myrotvorets.center/
+ * License: MIT
+ */
 
 // @codeCoverageIgnoreStart
 if ( defined( 'ABSPATH' ) ) {
