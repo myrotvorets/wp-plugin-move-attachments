0d283ab7a94e759e20f8ebdb4b4b9b14aa865a4e
Myrotvorets <support@myrotvorets.center>
2024-02-11 12:14:09 +0200

    Drop VENDOR_PATH

--- index.php
+++ index.php
@@ -11,10 +11,7 @@
 
 // @codeCoverageIgnoreStart
 if ( defined( 'ABSPATH' ) ) {
-	if ( defined( 'VENDOR_PATH' ) ) {
-		/** @psalm-suppress UnresolvableInclude, MixedOperand */
-		require_once constant( 'VENDOR_PATH' ) . '/vendor/autoload.php'; // NOSONAR
-	} elseif ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 		require_once __DIR__ . '/vendor/autoload.php';
 	} elseif ( file_exists( ABSPATH . 'vendor/autoload.php' ) ) {
 		require_once ABSPATH . 'vendor/autoload.php';
